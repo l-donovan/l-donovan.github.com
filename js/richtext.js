a2db25534bea0d3a682390a5073aab6415642d73
l-donovan <ladbuilder1@gmail.com>
2015-08-13 14:14:47 -0400

    fixed document naming

--- js/richtext.js
+++ js/richtext.js
@@ -1,19 +1,19 @@
 var QueryString = function () {
-  var query_string = {};
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if (typeof query_string[pair[0]] === "undefined") {
-      query_string[pair[0]] = decodeURIComponent(pair[1]);
-    } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
-      query_string[pair[0]] = arr;
-    } else {
-      query_string[pair[0]].push(decodeURIComponent(pair[1]));
-    }
-  }
-    return query_string;
+	var query_string = {};
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split("=");
+		if (typeof query_string[pair[0]] === "undefined") {
+			query_string[pair[0]] = decodeURIComponent(pair[1]);
+		} else if (typeof query_string[pair[0]] === "string") {
+			var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+			query_string[pair[0]] = arr;
+		} else {
+			query_string[pair[0]].push(decodeURIComponent(pair[1]));
+		}
+	}
+	return query_string;
 }();
 
 if (typeof QueryString.doc_loc !== 'undefined') {
@@ -22,26 +22,26 @@ if (typeof QueryString.doc_loc !== 'undefined') {
 	else
 		$("#docname").text(QueryString.doc_loc);
 	$.ajax({
-    	type: 'GET',
-    	url: QueryString.doc_loc,
-    	success: function (file_html) {
-    	    $("#document-text").html(file_html);
-    	}
+		type: 'GET',
+		url: QueryString.doc_loc,
+		success: function (file_html) {
+			$("#document-text").html(file_html);
+		}
 	});
 } else {
 	$("#docname").css("font-style", "italic");
 	$("#docname").text("Start Page");
 	$.ajax({
-    	type: 'GET',
-    	url: '/startpage',
-    	success: function (file_html) {
-    	    $("#document-text").html(file_html);
-    	}
+		type: 'GET',
+		url: '/startpage',
+		success: function (file_html) {
+			$("#document-text").html(file_html);
+		}
 	});
 }
 
 function promptDocName() {
-	var name = prompt("Document Name:");
+	var name = prompt("Document Name:", $("#docname").text());
 	if (name !== null)
 		$("#docname").text(name);
 }
