a373430372439be34ee1201d99f036dcd02ea36c
l-donovan <ladbuilder1@gmail.com>
2015-08-15 14:59:45 -0400

    modified header, added b/i/u

--- js/richtext.js
+++ js/richtext.js
@@ -17,9 +17,10 @@ var QueryString = function () {
 }();
 
 if (typeof QueryString.doc_loc !== 'undefined') {
-	if (typeof QueryString.doc_name !== 'undefined')
+	if (typeof QueryString.doc_name !== 'undefined') {
 		$("#docname").text(QueryString.doc_name);
-	else
+		document.title = QueryString.doc_name;
+	} else
 		$("#docname").text(QueryString.doc_loc);
 	$.ajax({
 		type: 'GET',
@@ -40,8 +41,45 @@ if (typeof QueryString.doc_loc !== 'undefined') {
 	});
 }
 
+var b_=$("#bold")[0];b_.onmousedown=function(e){e=e||window.event;e.preventDefault();};
+var i_=$("#italic")[0];i_.onmousedown=function(e){e=e||window.event;e.preventDefault();};
+var u_=$("#underline")[0];u_.onmousedown=function(e){e=e||window.event;e.preventDefault();};
+
 function promptDocName() {
 	var name = prompt("Document Name:", $("#docname").text());
 	if (name !== null)
 		$("#docname").text(name);
+}
+
+function bold(elem) {
+	document.execCommand('bold');
+	$('#bold').toggleClass('down');
+}
+
+function italic(elem) {
+	document.execCommand('italic');
+	$('#italic').toggleClass('down');
+}
+
+function underline(elem) {
+	document.execCommand('underline');
+	$('#underline').toggleClass('down');
+}
+
+function selectionIsBold() {
+	if (document.queryCommandState)
+		return document.queryCommandState('bold');
+	return false;
+}
+
+function selectionIsItalic() {
+	if (document.queryCommandState)
+		return document.queryCommandState('italic');
+	return false;
+}
+
+function selectionIsUnderlined() {
+	if (document.queryCommandState)
+		return document.queryCommandState('underline');
+	return false;
 }
